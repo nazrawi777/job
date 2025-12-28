96111745416871e4980a1cf9be0c92fad9734866
nazrawi777 <naxi8297@gmail.com>
2025-12-28 06:35:37 -0800

    first commit

---
+++ js/plugins.init.js
@@ -0,0 +1,431 @@
+/* Template Name: Jobnova - Job Board & Job Portal Bootstrap 5 Template
+   Author: Shreethemes
+   Email: support@shreethemes.in
+   Website: https://shreethemes.in
+   Version: 1.1.0
+   Created: January 2022
+   File Description: Common JS file of the template(plugins.init.js)
+*/
+
+
+/***********************************/
+/*         INDEX                   */
+/*==================================
+ *     01.  Tiny Slider            *
+ *     02.  Countdown Js           * (For Comingsoon pages)
+ *     03.  Maintenance js         * (For Maintenance page)
+ *     04.  Tobii lightbox         * (For Portfolio pages)
+ *     05.  Data Counter           *
+ *     06.  Typed Text animation   *
+ *     07.  Choice js              *
+ *     08.  Upload Profile         *
+ *     09.  Switcher Pricing Plans *
+ *     10.  Switcher               *
+ ==================================*/
+         
+//=========================================//
+/*            01) Tiny slider              */
+//=========================================//
+if(document.getElementsByClassName('tiny-one-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-one-item',
+        items: 1,
+        controls: true,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
+        nav: false,
+        speed: 400,
+        gutter: 0,
+    });
+};
+
+if(document.getElementsByClassName('tiny-single-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-single-item',
+        items: 1,
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 0,
+    });
+};
+
+
+if(document.getElementsByClassName('tiny-three-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-three-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 3
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+if(document.getElementsByClassName('tiny-slide-three').length > 0) {
+    var slider = tns({
+        container: '.tiny-slide-three',
+        controls: true,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
+        nav: false,
+        speed: 400,
+        gutter: 0,
+        responsive: {
+            1025: {
+                items: 3
+            },
+
+            992: {
+                items: 2
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+
+if(document.getElementsByClassName('tiny-five-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-five-item',
+        controls: true,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
+        nav: false,
+        speed: 400,
+        gutter: 0,
+        responsive: {
+            1025: {
+                items: 5
+            },
+
+            992: {
+                items: 4
+            },
+
+            767: {
+                items: 3
+            },
+
+            425: {
+                items: 1
+            },
+        },
+    });
+};
+
+
+//=========================================//
+/*/*            02) Countdown js           */
+//=========================================//
+
+try {
+    if(document.getElementById("days")){
+        // The data/time we want to countdown to
+        var eventCountDown = new Date("December 25, 2026 16:37:52").getTime();
+
+        // Run myfunc every second
+        var myfunc = setInterval(function () {
+
+            var now = new Date().getTime();
+            var timeleft = eventCountDown - now;
+
+            // Calculating the days, hours, minutes and seconds left
+            var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+            // Result is output to the specific element
+            document.getElementById("days").innerHTML = days + "<p class='count-head'>Days</p> "
+            document.getElementById("hours").innerHTML = hours + "<p class='count-head'>Hours</p> "
+            document.getElementById("mins").innerHTML = minutes + "<p class='count-head'>Mins</p> "
+            document.getElementById("secs").innerHTML = seconds + "<p class='count-head'>Secs</p> "
+
+            // Display the message when countdown is over
+            if (timeleft < 0) {
+                clearInterval(myfunc);
+                document.getElementById("days").innerHTML = ""
+                document.getElementById("hours").innerHTML = ""
+                document.getElementById("mins").innerHTML = ""
+                document.getElementById("secs").innerHTML = ""
+                document.getElementById("end").innerHTML = "00:00:00:00";
+            }
+        }, 1000);
+    }
+} catch (err) {
+
+}
+
+
+//=========================================//
+/*/*            03) Maintenance js         */
+//=========================================//
+
+try {
+    if(document.getElementById("maintenance")){
+        var seconds = 3599;
+        function secondPassed() {
+            var minutes = Math.round((seconds - 30) / 60);
+            var remainingSeconds = seconds % 60;
+            if (remainingSeconds < 10) {
+                remainingSeconds = "0" + remainingSeconds;
+            }
+            document.getElementById('maintenance').innerHTML = minutes + ":" + remainingSeconds;
+            if (seconds == 0) {
+                clearInterval(countdownTimer);
+                document.getElementById('maintenance').innerHTML = "Buzz Buzz";
+            } else {
+                seconds--;
+            }
+        }
+        var countdownTimer = setInterval('secondPassed()', 1000);
+    }
+} catch (err) {
+
+}
+
+//=========================================//
+/*/*            04) Tobii lightbox         */
+//=========================================//
+
+try {
+    const tobii = new Tobii()
+} catch (err) {
+
+}
+
+//=========================================//
+/*/*            05) Data Counter           */
+//=========================================//
+
+try {
+    const counter = document.querySelectorAll('.counter-value');
+    const speed = 2500; // The lower the slower
+
+    counter.forEach(counter_value => {
+        const updateCount = () => {
+            const target = +counter_value.getAttribute('data-target');
+            const count = +counter_value.innerText;
+
+            // Lower inc to slow and higher to slow
+            var inc = target / speed;
+
+            if (inc < 1) {
+                inc = 1;
+            }
+
+            // Check if target is reached
+            if (count < target) {
+                // Add inc to count and output in counter_value
+                counter_value.innerText = (count + inc).toFixed(0);
+                // Call function every ms
+                setTimeout(updateCount, 1);
+            } else {
+                counter_value.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+} catch (err) {
+
+}
+
+
+//=========================================//
+/*/* 06) Typed Text animation (animation) */
+//=========================================//
+
+try {
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+        if (this.isDeleting) { delta /= 2; }
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    function typewrite() {
+        if (toRotate === 'undefined') {
+            changeText()
+        }
+        else
+            var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid transparent}";
+        document.body.appendChild(css);
+    };
+    window.onload(typewrite());
+} catch(err) {
+
+}
+
+//=========================================//
+/*            07) Choice js                */
+//=========================================//
+try {
+    var singleLocation = new Choices('#choices-location');
+    var singleCategorie = document.getElementById('choices-type');
+    if(singleCategorie){
+        var singleCategories = new Choices('#choices-type');
+    }
+} catch (error) {
+    
+}
+
+//=========================================//
+/*            08) Upload Profile           */
+//=========================================//
+try {
+    var loadFile = function (event) {
+        
+        var image = document.getElementById(event.target.name);
+        image.src = URL.createObjectURL(event.target.files[0]);
+    };
+      
+} catch (error) {
+    
+}
+
+//=========================================//
+/*/*      09) Switcher Pricing Plans       */
+//=========================================//
+try {
+    var e = document.getElementById("filt-monthly"),
+        d = document.getElementById("filt-yearly"),
+        t = document.getElementById("switcher"),
+        m = document.getElementById("monthly"),
+        y = document.getElementById("yearly");
+
+    e.addEventListener("click", function(){
+        t.checked = false;
+        e.classList.add("toggler--is-active");
+        d.classList.remove("toggler--is-active");
+        m.classList.remove("hide");
+        y.classList.add("hide");
+    });
+
+    d.addEventListener("click", function(){
+        t.checked = true;
+        d.classList.add("toggler--is-active");
+        e.classList.remove("toggler--is-active");
+        m.classList.add("hide");
+        y.classList.remove("hide");
+    });
+
+    t.addEventListener("click", function(){
+        d.classList.toggle("toggler--is-active");
+        e.classList.toggle("toggler--is-active");
+        m.classList.toggle("hide");
+        y.classList.toggle("hide");
+    })
+} catch(err) {
+
+}
+
+//=========================================//
+/*/*            10) Switcher               */
+//=========================================//
+try {
+    // Swicher
+    function toggleSwitcher() {
+        var i = document.getElementById('style-switcher');
+        if (i.style.left === "-189px") {
+            i.style.left = "0px";
+        } else {
+            i.style.left = "-189px";
+        }
+    };
+
+    function setTheme(theme) {
+        document.getElementById('theme-opt').href = './css/' + theme + '.min.css';
+        toggleSwitcher(false);
+    };
+} catch (error) {
+    
+}
